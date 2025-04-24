e36caa8915003142cf42140538ecae0f31b7ccab
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2025-04-24 15:44:28 +0200

    👍 IMPROVE: Blacklisting works as expected

--- src/components/PopUp/PopUp.tsx
+++ src/components/PopUp/PopUp.tsx
@@ -1,19 +1,22 @@
 const PopUp: React.FC = () => {
     const handleToggleAll = async () => {
         const extensions: chrome.management.ExtensionInfo[] = await chrome.management.getAll();
+        const { blacklist = [] }: { blacklist: string[] } = await chrome.storage.local.get('blacklist');
 
         const anyEnabled = extensions.some(
             (ext) =>
                 ext.enabled &&
                 ext.type === 'extension' &&
-                ext.id !== chrome.runtime.id
+                ext.id !== chrome.runtime.id &&
+                !blacklist.includes(ext.id)
         );
 
         for (const ext of extensions) {
             if (
                 ext.type === 'extension' &&
                 ext.id !== chrome.runtime.id &&
-                ext.mayDisable
+                ext.mayDisable &&
+                !blacklist.includes(ext.id)
             ) {
                 await chrome.management.setEnabled(ext.id, !anyEnabled);
             }
